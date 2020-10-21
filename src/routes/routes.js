e8ab1c5dd44ec56579d8cccfffecd46394262008
mugabodeo <mugabodeo6@gmail.com>
2020-10-21 13:12:23 +0200

    added verifying user before get and update user profile

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,11 +12,15 @@ export default routes;
 =======
 import welcome from '../controllers/welcome';
 import { getUserProfile, updateUserProfile } from '../controllers/userprofile';
+<<<<<<< HEAD
 import verifyUserToken from '../middlewares/userprofileverification';
 =======
 import indexRoutes from './index.routes';
 import profileRoute from './profile.routes';
 >>>>>>> separated data and business logic and added get all users endpoint
+=======
+import { verifyUserToken, verifyUser } from '../middlewares/userprofileverification';
+>>>>>>> added verifying user before get and update user profile
 
 const routes = express.Router();
 routes.use(indexRoutes);
@@ -31,7 +35,11 @@ router.get('/', welcome);
 router.get('/users/:userId', verifyUserToken, getUserProfile);
 
 // ------------------ update user profile --------
+<<<<<<< HEAD
 router.patch('/users/:userId', verifyUserToken, updateUserProfile);
+=======
+router.patch('/users/:userId', verifyUser, updateUserProfile);
+>>>>>>> added verifying user before get and update user profile
 
 export default router;
 >>>>>>> added verifying user before get and update user profile
