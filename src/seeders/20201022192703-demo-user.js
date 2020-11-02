d7d119597650fe9fb238fa48511c626c81ee4fee
mugabodeo <mugabodeo6@gmail.com>
2020-11-02 22:37:43 +0200

    fixing bugs

--- src/seeders/20201022192703-demo-user.js
+++ src/seeders/20201022192703-demo-user.js
@@ -3,15 +3,15 @@ const { date } = require("joi");
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Users', [{
-      first_name: 'Ineza',
-      last_name: 'Bonte',
+      first_name: 'Mugabo',
+      last_name: 'Deo',
+      username: 'mdeo1',
       occupation: 'software devlopment',
+      email: 'mdeo@gmail.com',
       password: '12345678',
       address: 'Kigali',
       language: 'English',
-      profile_picture: 'image.png',
-      createdAt: new Date(),
-      updatedAt: new Date()
+      profile_picture: 'image.png'
     }], {});
   },
 
--- tests/app.test.js
+++ tests/app.test.js
@@ -5,7 +5,7 @@ import app from '../src/app';
 use(chaiHttp);
 describe('testing welcome router', () => {
   it('Should get welcome message', async () => {
-    const res = await request(app).get('/');
+    const res = await request(app).get('/api/v1/');
     expect(res).to.have.status([200]);
     expect(res.type).to.equal('application/json');
     expect(res.body).to.have.property('message');
