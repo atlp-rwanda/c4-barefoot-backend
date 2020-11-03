b972cf6dd2662e2b0fc8481a8c5da0bfd6195cc5
mugabodeo <mugabodeo6@gmail.com>
2020-11-03 02:14:30 +0200

    fixed bugs

--- src/middlewares/userprofileverification.js
+++ src/middlewares/userprofileverification.js
@@ -4,18 +4,18 @@ import 'dotenv/config';
 // verify user token
 const verifyUserToken = async (req, res, next) => {
   try {
-    if (!req.headers.authorization) return res.status(401).json({ message: 'no auth header found' });
+    if (!req.headers.authorization) return res.status(401).json({ status: 401, message: 'no auth header found' });
     const authHeader = req.headers.authorization.split(' ');
     const [authString, token] = authHeader;
-    if (authString !== 'Bearer') return res.status(401).json({ message: 'no auth header found' });
-    if (!token) return res.status(401).json({ message: 'No token found' });
+    if (authString !== 'Bearer') return res.status(401).json({ status: 401, message: 'no auth header found' });
+    if (!token) return res.status(401).json({ status: 401, message: 'No token found' });
     const verify = jwt.verify(token, process.env.TOKEN_SECRET);
-    if (!verify) return res.status(401).json({ message: 'token cant be verified' });
+    if (!verify) return res.status(401).json({ status: 401, message: 'token cant be verified' });
     res.locals = token;
-    if (res.locals !== token) return res.status(500).json({ message: 'server cant assign token' });
+    if (res.locals !== token) return res.status(500).json({ status: 500, message: 'server cant assign token' });
     next();
   } catch (err) {
-    return res.status(401).json({ message: 'invalid user token' });
+    return res.status(401).json({ status: 401, message: 'invalid user token' });
   }
 };
 // eslint-disable-next-line import/prefer-default-export
