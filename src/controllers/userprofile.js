fabb0dd59719123974d8610dbc4c8ef3c382d89c
mugabodeo <mugabodeo6@gmail.com>
2020-11-04 20:28:23 +0200

    added routes and refactored class of errors

--- src/controllers/userprofile.js
+++ src/controllers/userprofile.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import bcrypt from 'bcrypt';
 import UserServices from '../services/user.service';
 <<<<<<< HEAD
@@ -20,11 +21,18 @@ const getAllUsers = async (req, res, next) => {
   } catch (err) { next(err); }
 };
 >>>>>>> added error handling
+=======
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import UserServices from '../services/user.service';
+import UsersError from '../utils/userserror';
+>>>>>>> added routes and refactored class of errors
 
 // get a user profile with either Id or first_name
 const getUserProfile = async (req, res, next) => {
   try {
     const { username } = req.params;
+<<<<<<< HEAD
 <<<<<<< HEAD
     if (!username) throw new UsersError('invalid URI', 404);
     const record = await UserServices.getUserByUserName(username);
@@ -34,6 +42,11 @@ const getUserProfile = async (req, res, next) => {
     const record = await UserServices.getUserByUserName(username);
     if (!record) throw new UpdateProfileError('user not found', 404);
 >>>>>>> added error handling
+=======
+    if (!username) throw new UsersError('invalid URI', 404);
+    const record = await UserServices.getUserByUserName(username);
+    if (!record) throw new UsersError('user not found', 404);
+>>>>>>> added routes and refactored class of errors
     res.status(200).json({ status: 200, message: 'successful got user profile', data: record });
   } catch (err) { next(err); }
 };
@@ -50,13 +63,18 @@ const updateUserProfile = async (req, res, next) => {
 =======
     if (!res.locals) return res.status(500).json({ status: 500, message: 'can not receive locals' });
     const decodedToken = jwt.decode(res.locals);
-    if (!decodedToken) throw new UpdateProfileError('no token found', 404);
+    if (!decodedToken) throw new UsersError('no token found', 404);
     const userId = decodedToken.data;
-    if (!userId) throw new UpdateProfileError('unable to obtain a payload in token', 404);
+    if (!userId) throw new UsersError('unable to obtain a payload in token', 404);
     const record = await UserServices.getUserById(userId);
+<<<<<<< HEAD
     if (!record) throw new UpdateProfileError('user not found', 404);
     if (record.dataValues.id !== userId) throw new UpdateProfileError('owner of profile does not match signed in user', 401);
 >>>>>>> added error handling
+=======
+    if (!record) throw new UsersError('user not found', 404);
+    if (record.dataValues.id !== userId) throw new UsersError('owner of profile does not match signed in user', 401);
+>>>>>>> added routes and refactored class of errors
     if (req.body.password) req.body.password = await bcrypt.hash(req.body.password, 10);
     UserServices.updateUser(req.body, email);
     res.status(200).json({ status: 200, message: 'successfully updated user profile' });
