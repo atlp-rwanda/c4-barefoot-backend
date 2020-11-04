02ba47d4e196f3107c54e8500765ffb9adb45b2a
mugabodeo <mugabodeo6@gmail.com>
2020-11-04 21:24:48 +0200

    fix userId URI bugs in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,7 @@ const routes = express.Router();
 routes.use('/user', userRoute);
 routes.use('/', landingPage);
 routes.use('/update-profile', updateUserProfile);
-routes.use('/:username', getUserProfile);
+routes.use('/', getUserProfile);
 routes.use('/users', getAllUsers);
 
 export default routes;
