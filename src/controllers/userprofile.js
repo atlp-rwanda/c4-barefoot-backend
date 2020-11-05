a3f2102053e02e76cf881fbd4701f798b6f79914
mugabodeo <mugabodeo6@gmail.com>
2020-11-05 17:26:54 +0200

    added tests for updating user endpoint

--- src/controllers/userprofile.js
+++ src/controllers/userprofile.js
@@ -1,4 +1,3 @@
-import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import UserServices from '../services/user.service';
 import UsersError from '../utils/userserror';
@@ -9,7 +8,7 @@ const getUserProfile = async (req, res, next) => {
     const { username } = req.params;
     if (!username) throw new UsersError('invalid URI', 404);
     const record = await UserServices.getUserByUserName(username);
-    if (!record) throw new UsersError('user not found', 404);
+    if (!record) throw new UsersError('user not found', 400);
     res.status(200).json({ status: 200, message: 'successful got user profile', data: record });
   } catch (err) { next(err); }
 };
@@ -17,25 +16,13 @@ const getUserProfile = async (req, res, next) => {
 // update a single user profile
 const updateUserProfile = async (req, res, next) => {
   try {
-    if (!res.locals) return res.status(500).json({ status: 500, message: 'can not receive locals' });
-    const decodedToken = jwt.decode(res.locals);
-    if (!decodedToken) throw new UsersError('no token found', 404);
-    const userId = decodedToken.data;
-    if (!userId) throw new UsersError('unable to obtain a payload in token', 404);
+    if (!res.locals.token) return res.status(500).json({ status: 500, message: 'unable to obtain a payload in token' });
+    const userId = res.locals.token;
     const record = await UserServices.getUserById(userId);
     if (!record) throw new UsersError('user not found', 404);
     if (record.dataValues.id !== userId) throw new UsersError('owner of profile does not match signed in user', 401);
     if (req.body.password) req.body.password = await bcrypt.hash(req.body.password, 10);
-    const data = {
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      username: req.body.username,
-      password: req.body.password,
-      address: req.body.address,
-      language: req.body.language,
-      profile_picture: req.body.profile_picture
-    };
-    await UserServices.updateUser(data, userId);
+    UserServices.updateUser(req.body, userId);
     res.status(200).json({ status: 200, message: 'successfully updated user profile' });
   } catch (err) { next(err); }
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import landingPage from './api/landingPageRoute';
-import updateUserProfile from './api/updateprofileroute';
-import getUserProfile from './api/userprofileroute';
+import userProfile from './api/userprofile';
 import userRoute from './api/user';
 
 const routes = express.Router();
 
 routes.use('/user', userRoute);
 routes.use('/', landingPage);
-routes.use('/update-profile', updateUserProfile);
-routes.use('/', getUserProfile);
+routes.use('/', userProfile);
 
 export default routes;
