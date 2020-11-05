d90829bdc239d9f35f27bddcd236d60b7df7cd82
mugabodeo <mugabodeo6@gmail.com>
2020-11-05 18:52:36 +0200

    refactored code to use email as data in token

--- src/controllers/userprofile.js
+++ src/controllers/userprofile.js
@@ -17,12 +17,12 @@ const getUserProfile = async (req, res, next) => {
 const updateUserProfile = async (req, res, next) => {
   try {
     if (!res.locals.token) return res.status(500).json({ status: 500, message: 'unable to obtain a payload in token' });
-    const userId = res.locals.token;
-    const record = await UserServices.getUserById(userId);
+    const email = res.locals.token;
+    const record = await UserServices.getUserByEmail(email);
     if (!record) throw new UsersError('user not found', 404);
-    if (record.dataValues.id !== userId) throw new UsersError('owner of profile does not match signed in user', 401);
+    if (record.dataValues.email !== email) throw new UsersError('owner of profile does not match signed in user', 401);
     if (req.body.password) req.body.password = await bcrypt.hash(req.body.password, 10);
-    UserServices.updateUser(req.body, userId);
+    UserServices.updateUser(req.body, email);
     res.status(200).json({ status: 200, message: 'successfully updated user profile' });
   } catch (err) { next(err); }
 };
