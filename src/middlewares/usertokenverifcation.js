062477540e61f5c949f6d52b75d711187b25fc37
mugabodeo <mugabodeo6@gmail.com>
2020-11-05 12:53:39 +0200

    add testing for verifyuser token middleware

--- src/middlewares/usertokenverifcation.js
+++ src/middlewares/usertokenverifcation.js
@@ -2,9 +2,13 @@ import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 import VerifyTokenError from '../utils/verifytokenerror';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import UsersError from '../utils/userserror';
 =======
 >>>>>>> refactoring controllers and middlewares
+=======
+import UsersError from '../utils/userserror';
+>>>>>>> add testing for verifyuser token middleware
 import UserServices from '../services/user.service';
 // verify user token
 const verifyUserToken = async (req, res, next) => {
@@ -24,10 +28,16 @@ const verifyUserToken = async (req, res, next) => {
     if (!res.locals.token) throw new VerifyTokenError('server cant assign token', 500);
 =======
     const record = await UserServices.getUserById(decodedToken.data);
+<<<<<<< HEAD
     if (!record) throw new VerifyTokenError('data in token is invalid', 401);
     res.locals = token;
     if (res.locals !== token) throw new VerifyTokenError('server cant assign token', 500);
 >>>>>>> refactoring controllers and middlewares
+=======
+    if (!record) throw new UsersError('data in token is invalid', 400);
+    res.locals.token = await decodedToken.data;
+    if (!res.locals.token) throw new VerifyTokenError('server cant assign token', 500);
+>>>>>>> add testing for verifyuser token middleware
     next();
   } catch (err) {
     next(err);
