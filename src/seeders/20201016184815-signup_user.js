a68c47b6a96cf3bb18c71c8ff58b9b317b41afa4
mugabodeo <mugabodeo6@gmail.com>
2020-11-05 17:19:59 +0200

    added occupation field

--- src/seeders/20201016184815-signup_user.js
+++ src/seeders/20201016184815-signup_user.js
@@ -1,13 +1,14 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('users', [{
-      first_name: 'Ineza',
-      last_name: 'Bonte',
-      username: 'InezaBon',
-      email: 'inezabonte@gmail.com',
-      password: '12345678',
-      address: 'Kigali',
-      language: 'English',
+      first_name: 'Mugabo',
+      last_name: 'Deo',
+      username: 'Mdeo12',
+      occupation: 'software devlopment',
+      email: 'mdeo@gmail.com',
+      password: 'pass123',
+      address: 'Nairobi',
+      language: 'French',
       profile_picture: 'image.png'
     }], {});
   },
