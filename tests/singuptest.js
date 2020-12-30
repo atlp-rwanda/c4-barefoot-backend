55d51dea0af76548741ca9092baa387d4152c847
Blaiseniyo <blaiseniyonkuru12@gmail.com>
2020-12-30 18:31:22 +0200

    adding signin tests

---
+++ tests/singuptest.js
@@ -0,0 +1,54 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import sinon from 'sinon';
+import {successSignUp,signUp,failedSignIn} from '../controllers/googleSignup'
+import app from '../app';
+const expect = chai.expect;
+
+
+describe('#Sign Up with Google',()=>{
+  let status ,send, res;
+    beforeEach((done) => {
+      // status = sinon.stub();
+      send = sinon.spy();
+      res = { send, status };
+      // status.returns(res);
+      done();
+    });
+    it('It should signUp a user with his google account',async(done)=>{
+        let req={user:{
+          provider: 'google',
+          sub: '106180666226862347646',
+          id: '106180666226862347646',
+          displayName: 'Niyonkuru Blaise',
+          name: { givenName: 'Niyonkuru', familyName: 'Blaise' },
+          given_name: 'Niyonkuru',
+          family_name: 'Blaise',
+          email_verified: true,
+          verified: true,
+          language: 'en',
+          locale: undefined,
+          email: 'blaiseniyonkuru12@gmail.com',
+          emails: [ { value: 'blaiseniyonkuru12@gmail.com', type: 'account' } ],
+          photos: [
+            {
+              value: 'https://lh3.googleusercontent.com/a-/AOh14Ghe0RBclUzQrw32zJrUjIY-uN7XjLnYNr4SIX7U=s96-c',
+              type: 'default'
+            }
+          ]
+        }
+        }
+        const mock =sinon.mock(res);
+
+        mock.expects('send').once().withExactArgs({message:'successfully Logged In'})
+         const result = await signUp(req,res)
+         done()
+         const stub =sinon.stub(signUp(req,res)).returns({message:'successfully Logged In',
+        token:result.token})
+          expect(stub.calledOnce).to.be.true;
+          expect(stub.args[0],message).to.equal("successfully Logged In")
+          mock.verify();
+          
+          
+    })
+})
