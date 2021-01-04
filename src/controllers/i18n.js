4060b024cb5fb6d01aa7ca324cbe3283d928b180
songa1 <songachillethe1her@gmail.com>
2021-01-04 11:20:36 +0200

    Translation is working and what left is to add all left translations

--- src/controllers/i18n.js
+++ src/controllers/i18n.js
@@ -2,7 +2,7 @@ import i18n from 'i18n';
 import path from 'path';
 
 i18n.configure({
-    locales: ['en', 'fr'],
+    locales: ['en', 'fr', 'kin'],
     directory: path.join('./src/controllers', 'locales'),
     autoReload: true,
     defaultLocale: 'en',
