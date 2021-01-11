7f59d7a535813faa6f23431ad228dd5a00034a21
Mungeri Frank <mungerifrank@gmail.com>
2021-01-11 22:46:11 +0200

    modifies chat table to include type property

--- src/models/chat.js
+++ src/models/chat.js
@@ -1,49 +1,34 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Chat extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-    toJSON(){
-      return {...this.get(), id: undefined}
-    }
-  };
-  Chat.init({
-    uuid: {
+  const Chat = sequelize.define('Chat', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
       type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4
+      defaultValue: DataTypes.UUIDV4,
     },
     sender: {
-      type:DataTypes.UUID,
-      allowNull:false
+      type: DataTypes.UUID,
+      allowNull: false
     },
     receiver: {
-      type:DataTypes.UUID,
-      allowNull:false
+      type: DataTypes.UUID,
+      allowNull: false
     },
     message: {
-      type:DataTypes.TEXT
-      
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    file: {
-      type:DataTypes.STRING
-    
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'plain-text'
     },
     status: {
-      type:DataTypes.BOOLEAN,
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
       defaultValue: false
     }
-  }, {
-    sequelize,
-    modelName: 'Chat',
   });
+
   return Chat;
 };
--- src/seeders/20210107153655-ChatV.js
+++ src/seeders/20210107153655-ChatV.js
@@ -1,89 +1,107 @@
 module.exports = {
-    up: async (queryInterface, Sequelize) => {
-      await queryInterface.bulkInsert('ChatVs', [{
-        id: '0ce36391-2c08-3074-bddb-a4ea8bbcbba6',
-        visitor: 'visitor@yahoo.com',
-        message: 'Hello Here!',
-        sender: 'visitor@yahoo.com',
-        status: true,
-        createdAt: '2021-01-01 12:07:22.717+02',
-        updatedAt: '2021-01-01 12:07:22.717+02'
-      },
-      {
-        id: '0ce36391-2c08-3074-bddb-a4ea8bbcbbc7',
-        visitor: 'visitor@yahoo.com',
-        message: 'Yes Dear, How can we help you?',
-        sender: 'superadmin@gmail.com',
-        status: true,
-        createdAt: '2021-01-01 12:07:42.717+02',
-        updatedAt: '2021-01-01 12:07:42.717+02',
-      },
-      {
-        id: '0ce36391-2c08-3074-bddb-a4e78bbcbbc6',
-        visitor: 'visitor@yahoo.com',
-        message: 'Do you have accomodations in Nigeria ?',
-        sender: 'visitor@yahoo.com',
-        status: true,
-        createdAt: '2021-01-02 10:28:20.717+02',
-        updatedAt: '2021-01-02 10:28:20.717+02'
-      },
-      {
-        id: '0ce36391-2c08-3074-bddb-a4eacbbcbbc6',
-        visitor: 'visitor@yahoo.com',
-        message: 'Since January 2021, We have been able to accomodate 50 people in our system and the number is expected to increase gradually by the end of this year.',
-        sender: 'traveladmin@gmail.com',
-        status: true,
-        createdAt: '2021-01-02 10:28:22.717+02',
-        updatedAt: '2021-01-02 10:28:22.717+02'
-      },
-      {
-        id: '0ce36391-2c08-3074-bddb-a4ea8bacbbc6',
-        visitor: 'visitor@yahoo.com',
-        message: 'Great! Now help me to signup with my yahoo. I do not see `Signin with Yahoo`. Am I not eligible here ?',
-        sender: 'visitor@yahoo.com',
-        status: true,
-        createdAt: '2021-01-02 10:28:25.717+02',
-        updatedAt: '2021-01-02 10:28:25.717+02'
-      },
-      {
-        id: '0ce36391-2c08-3074-bddb-a4ea8b3cbbc6',
-        visitor: 'visitor@yahoo.com',
-        message: 'Currently we allow signup with Google, Facebook and the traditional signup. Since we do not have the yahoo signup/signin provide, please SignUp with the traditional option.',
-        sender: 'superadmin@gmail.com',
-        status: false,
-        createdAt: '2021-01-02 10:28:26.717+02',
-        updatedAt: '2021-01-02 10:28:26.717+02'
-      },
-      {
-        id: '0ce36391-2c08-3074-bddb-a4ea8bbcbdc6',
-        visitor: 'otherVisitor@gmail.com',
-        message: 'Hello Support!',
-        sender: 'otherVisitor@gmail.com',
-        status: true,
-        createdAt: '2021-01-02 10:28:27.717+02',
-        updatedAt: '2021-01-02 10:28:27.717+02'
-      },
-      {
-        id: '0ce36391-2f08-3074-bddb-a4ea8bbcbdc6',
-        visitor: 'otherVisitor@gmail.com',
-        message: 'Yes dear, how can we help you today ?',
-        sender: 'superadmin@gmail.com',
-        status: true,
-        createdAt: '2021-01-02 10:29:27.717+02',
-        updatedAt: '2021-01-02 10:29:27.717+02'
-      },
-      {
-        id: '0ce36391-2d08-3074-bddb-a4ea8bbcbdc6',
-        visitor: 'otherVisitor@gmail.com',
-        message: 'My iPhone 11 Pro is not working :\'(',
-        sender: 'otherVisitor@gmail.com',
-        status: false,
-        createdAt: '2021-01-02 10:39:27.717+02',
-        updatedAt: '2021-01-02 10:39:27.717+02'
-      }]);
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('ChatVs', [{
+      id: '916ae5eb-2022-4d42-a859-19f6e557d4eb',
+      visitor: 'visitor@yahoo.com',
+      sender: 'visitor@yahoo.com',
+      message: 'Hello Here!',
+      type: 'plain-text',
+      status: true,
+      createdAt: '2021-01-01T12:07:22.717Z',
+      updatedAt: '2021-01-01T12:07:22.717Z'
     },
-    down: async (queryInterface, Sequelize) => {
-      await queryInterface.bulkDelete('ChatVs', null, {});
-    }
-  };
-  
+    {
+      id: 'f8f7ceec-5222-46ae-bd06-677ef7b0c0e6',
+      visitor: 'visitor@yahoo.com',
+      sender: 'superadmin@gmail.com',
+      message: 'Yes Dear, How can we help you?',
+      type: 'plain-text',
+      status: true,
+      createdAt: '2021-01-01T12:07:42.717Z',
+      updatedAt: '2021-01-01T12:07:42.717Z',
+    },
+    {
+      id: 'd1ebb771-88fd-4543-9055-852abda33ce4',
+      visitor: 'visitor@yahoo.com',
+      sender: 'visitor@yahoo.com',
+      message: 'Do you have accomodations in Nigeria ?',
+      type: 'plain-text',
+      status: true,
+      createdAt: '2021-01-02T10:28:20.717Z',
+      updatedAt: '2021-01-02T10:28:20.717Z'
+    },
+    {
+      id: '7865a19c-27a7-4a78-bea6-9f806f6dc45c',
+      visitor: 'visitor@yahoo.com',
+      sender: 'traveladmin@gmail.com',
+      message: 'Since January 2021, We have been able to accomodate 50 people in our system and the number is expected to increase gradually by the end of this year. You can refer to the PDF below',
+      type: 'plain-text',
+      status: true,
+      createdAt: '2021-01-02T10:28:22.717Z',
+      updatedAt: '2021-01-02T10:28:22.717Z'
+    },
+    {
+      id: '147a12ce-d89b-45f1-ae1f-d1de6b9da416',
+      visitor: 'visitor@yahoo.com',
+      sender: 'traveladmin@gmail.com',
+      message: 'https://www.unicode.org/L2/L2015/15140-utr51-2d9.pdf',
+      type: 'application/pdf',
+      status: true,
+      createdAt: '2021-01-02T10:28:22.717Z',
+      updatedAt: '2021-01-02T10:28:22.717Z'
+    },
+    {
+      id: '6f33c2ef-4cdd-4dd4-aedc-daa00d9dcd4c',
+      visitor: 'visitor@yahoo.com',
+      sender: 'visitor@yahoo.com',
+      message: 'Great! Now help me to signup with my yahoo. I do not see `Signin with Yahoo`. Am I not eligible here ?',
+      type: 'plain-text',
+      status: true,
+      createdAt: '2021-01-02T10:28:25.717Z',
+      updatedAt: '2021-01-02T10:28:25.717Z'
+    },
+    {
+      id: '5d75e557-10d3-497c-9a72-bee2196b4b64',
+      visitor: 'visitor@yahoo.com',
+      sender: 'superadmin@gmail.com',
+      message: 'Currently we allow signup with Google, Facebook and the traditional signup. Since we do not have the yahoo signup/signin provide, please SignUp with the traditional option.',
+      type: 'plain-text',
+      status: false,
+      createdAt: '2021-01-02T10:28:26.717Z',
+      updatedAt: '2021-01-02T10:28:26.717Z'
+    },
+    {
+      id: '69f4d340-ccfd-4ad5-85cd-90588a4454f2',
+      visitor: 'otherVisitor@gmail.com',
+      sender: 'otherVisitor@gmail.com',
+      message: 'Hello Support!',
+      type: 'plain-text',
+      status: true,
+      createdAt: '2021-01-02T10:28:27.717Z',
+      updatedAt: '2021-01-02T10:28:27.717Z'
+    },
+    {
+      id: '03c22ce1-000f-4281-91a0-d9280b4acb47',
+      visitor: 'otherVisitor@gmail.com',
+      sender: 'superadmin@gmail.com',
+      message: 'Yes dear, how can we help you today ?',
+      type: 'plain-text',
+      status: true,
+      createdAt: '2021-01-02T10:29:27.717Z',
+      updatedAt: '2021-01-02T10:29:27.717Z'
+    },
+    {
+      id: '2391257e-be0c-4ccb-ab52-65e5e88dfb7b',
+      visitor: 'otherVisitor@gmail.com',
+      sender: 'otherVisitor@gmail.com',
+      message: 'My iPhone 11 Pro is not working 😭',
+      type: 'plain-text',
+      status: false,
+      createdAt: '2021-01-02T10:39:27.717Z',
+      updatedAt: '2021-01-02T10:39:27.717Z'
+    }]);
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('ChatVs', null, {});
+  }
+};
