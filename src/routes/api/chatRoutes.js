46d028cd303069de720f8232959a57518342fac7
Mungeri Frank <mungerifrank@gmail.com>
2021-01-12 10:38:35 +0200

    ft(chat): refactor and add visitor endpoint for getting response

--- src/routes/api/chatRoutes.js
+++ src/routes/api/chatRoutes.js
@@ -3,15 +3,15 @@ import verifyUserToken from '../../middlewares/usertokenverification';
 import {
   getChatList, getChatsBetweenTwoUsers, getUsersToChatWith, postChat, markAsRead,
   deleteChatMessage, visitorMessage, supportResponse, readAsVisitor, readAsSupport,
-  getChatsV, getLastMessageBetweenTwo, getVisitorsList, getUnreadMessages
+  getChatsV, getLastMessageBetweenTwo, getVisitorsList, getUnreadMessages, visitorGetsChatVs
 } from '../../controllers/chatController';
 
 const router = express.Router();
 
-// loading users for the first time
+// get users to chat for the first time / or find new users to chat
 router.get('/users', verifyUserToken, getUsersToChatWith);
 
-// router.get('/recentChats', verifyUserToken, getRecentChatUsers);
+// get messages between two registered users
 router.get('/', verifyUserToken, getChatsBetweenTwoUsers);
 
 // get number of unreads between two users
@@ -20,25 +20,28 @@ router.get('/unread', verifyUserToken, getUnreadMessages);
 // get last message between two users
 router.get('/last', verifyUserToken, getLastMessageBetweenTwo);
 
-// getting chats for one user
+// get chatlist for one user
 router.get('/chatlist', verifyUserToken, getChatList);
 
-// loading users you recently chatted with
+// post / send your message to other registered user
 router.post('/', verifyUserToken, postChat);
 
-// mark the message status to `true` by receiver
+// mark the message as read ( status to `true` ) by receiver
 router.patch('/read', verifyUserToken, markAsRead);
 
-// delete a chat
+// delete a chat if you are the sender
 router.delete('/', verifyUserToken, deleteChatMessage);
 
-// visitor's messages incoming
+// visitor sends message to support
 router.post('/visitor', visitorMessage);
+
+// visitor gets messages from support
+router.get('/visitor', visitorGetsChatVs);
 
 // visitor reads the support message
 router.patch('/visitor', readAsVisitor);
 
-// support's response to visitor
+// support responds to visitor
 router.post('/support', verifyUserToken, supportResponse);
 
 // support reads the visitor message
@@ -48,6 +51,6 @@ router.patch('/support', verifyUserToken, readAsSupport);
 router.get('/visitors', verifyUserToken, getVisitorsList);
 
 // support gets the visitor's message
-router.get('/visitor', verifyUserToken, getChatsV);
+router.get('/support', verifyUserToken, getChatsV);
 
 export default router;
