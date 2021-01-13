2cd2d1b17ac1b0488b6eda309bcb36c4eb33a604
Alice-Gatera <alicegatera15@gmail.com>
2021-01-13 15:16:40 +0200

    fix code conflicts and codeclimate issues

--- src/controllers/statistics.js
+++ src/controllers/statistics.js
@@ -1,33 +1,35 @@
 import models from '../models'
-import top5VisitedLocation from '../helper/top5visitedloc'
-import top5BookedAccommodation from '../helper/top5VisitedBookedAcc'
-
+import sequelize from 'sequelize'
 const getStatistics = async function (req, res, next) {
     const { Op } = require("sequelize");
     const activeUsers = await models.User.findAll({
-        where: {
-          manager_id: {
-            [Op.not]: null
-          }
-        }
-        })
+        where: { manager_id: { [Op.not]: null } } })
+  // get top visited Location
+    const topVisitedLocation =  await models.Location.findAll({
+                attributes: ['LocationName',[sequelize.fn('COUNT',sequelize.col('LocationName')),'visits']],
+                group: ['LocationName'],
+                order:[[sequelize.fn('COUNT', sequelize.col('LocationName')), 'DESC']],
+                limit:3 // You can change 3 to any number you want
+             })
+            //Get top booked accommodation
+            const topBookedccommodationId =  await models.Booking.findAll({
+                attributes: ['accommodationId',[sequelize.fn('COUNT',sequelize.col('accommodationId')),'bookings']],
+                group: ['accommodationId'],
+                order:[[sequelize.fn('COUNT', sequelize.col('accommodationId')), 'DESC']],
+                limit:3 // You can change 3 to any number you want
+            })
         //Get the number of  Accomodation
     const numberOfAccommodation = await models.Accommodation.findAndCountAll({})
     //Get the number of Locations
     const numberOfLocation = await models.Location.findAndCountAll({})
-    // Get top 5 visited Location 
-    let sortedLocations = await top5VisitedLocation()
-    // Get top 5 visited, Booked Accommodation
-    let sortedAccomodation =await  top5BookedAccommodation()
     res.status(200).send({
         title: 'STATISTICS',
         numberOfActiveUsers: activeUsers.length,
         users: activeUsers,
         numberOfAccommodation: numberOfAccommodation.count,
         numberOfLocation,
-        mostVisitedLocations: sortedLocations.slice(0, 5),
-        top5VisitedBookedAccommodations: sortedAccomodation.slice(0,5)
-    })
-}
+        topVisitedLocation,
+        topBookedccommodationId 
+    }) }
 module.exports = getStatistics
 
