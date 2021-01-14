f3b1c9e18755806ffcca454debc4f5c49739f20b
Cyusa144 <69196120+Cyusa144@users.noreply.github.com>
2021-01-14 17:22:31 +0200

    gives permission to traveladmin

--- src/controllers/bookingsController.js
+++ src/controllers/bookingsController.js
@@ -8,7 +8,7 @@ const showBookings = async (req, res, next) => {
     const user = await getUserData(req, res);
     const booking = await models.Booking.findAll({ where: { username: user.username } });
     if (!booking[0]) {
-      throw new notFound(res.__('You do not have any bookings'));
+      res.json('You do not have any bookings');
     }
     res.status(200).json(booking);
   } catch (error) {
--- src/routes/api/bookingsRoute.js
+++ src/routes/api/bookingsRoute.js
@@ -2,8 +2,9 @@ import express from 'express';
 import { showBookings } from '../../controllers/bookingsController';
 import permit from '../../middlewares/accessControl';
 
+
 const router = express.Router();
 
-router.get('/', permit(['book accommodations']), showBookings);
+router.get('/', permit (['book accommodations']),showBookings );
 
 export default router;
