1ab1d21b2a0e4a4c6595ac7bdb9d974c7725bbeb
Alice-Gatera <alicegatera15@gmail.com>
2021-01-18 10:09:36 +0200

    fix code conflicts

--- src/helper/isAdmin.js
+++ src/helper/isAdmin.js
@@ -3,7 +3,6 @@ import findRoles from '../services/findRoles';
 import UnauthorizedError from '../utils/Errors/authorizationError';
 
 const isAdmin = async (req, res, next) => {
-<<<<<<< HEAD
     const bearerToken = req.headers.authorization;
     if (!bearerToken) throw new UnauthorizedError('Unauthorized');
     const token = bearerToken.split(' ')[1];
@@ -12,15 +11,6 @@ const isAdmin = async (req, res, next) => {
     if (roles.name === 'travel-admin' || roles.name === 'administrator') return next();
     if (roles.name !== 'travel-admin' || roles.name !== 'administrator') throw new UnauthorizedError('Access denied');
 
-=======
-  const bearerToken = req.headers.authorization;
-  if (!bearerToken) throw new UnauthorizedError('Unauthorized');
-  const token = bearerToken.split(' ')[1];
-  const decoded = await verifyToken(token);
-  const roles = await findRoles(decoded.role);
-  if  (roles.name === 'travel-admin' || roles.name==='administrator') return next();
-  if (roles.name !== 'travel-admin' || roles.name!=='administrator') throw new UnauthorizedError('Access denied');
->>>>>>> aeb91b35b08ec607d9c5eb7d808424d3c4975e49
 };
 // user.user_role_id
 
--- src/seeders/20201123091449-bookings.js
+++ src/seeders/20201123091449-bookings.js
@@ -16,33 +16,14 @@ export default {
         From:'2020-10-10',
         To:'2021-10-10'
         },
-        {
-          id: '0ce36391-2c08-3074-bddb-a4ea8cccbbc6',
-          username: 'requesterOne',
-          accommodationId:'0ce36391-2c08-3074-bddb-a4ea8cccbbc5',
-          From:'2020-10-10',
-          To:'2021-10-10'
-          },
-          {
-            id: '0ce36391-2c08-3074-bddb-a4ea8cccbbd9',
-            username: 'requesterOne',
-            accommodationId:'0ce36391-2c08-3074-bddb-a4ea8cccbbc5',
-            From:'2020-10-10',
-            To:'2021-10-10'
-            },
             {
               id: 'cf701c80-a729-4922-a04c-553015514a96',
               username: 'requesterOne',
               accommodationId:'520f2b37-7bac-4490-aa7a-96f15915bcd7',
               From:'2020-10-10',
               To:'2021-10-10'
-              },
-              
-
-    ], {});
-  
+              },], {});
   },
-
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Bookings', null, {});
   }
