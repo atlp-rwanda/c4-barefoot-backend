7bafda40e6a4b96d531360426973b4c14b2eb138
edgar929 <shumbushoedgar@gmail.com>
2021-01-19 14:11:23 +0200

    fixing refresh token

--- src/controllers/userController/refreshToken.js
+++ src/controllers/userController/refreshToken.js
@@ -23,7 +23,10 @@ const refreshToken = async (req, res, next) => {
       throw new BadRequestError(res.__('no user found with this token'), 400);
     }
     const userToken = generateToken(userData);
-
+    
+    await newUser.update({ refreshtoken: userToken });
+    res.clearCookie('make', { path: '/api/v1/user/refresh-token' });
+   
     // user.reftoken = reftoken;
     res.cookie('make', userToken, { httpOnly: false, path: '/api/v1/user/refresh-token' });
     return res.status(200).json({ userToken });
