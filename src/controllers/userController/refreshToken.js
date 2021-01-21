b8c728e2da09a9603d1dcef7b27ddbb0346a511f
edgar929 <56983661+edgar929@users.noreply.github.com>
2021-01-21 16:07:06 +0200

    fixing refresh token (#21)
    * fixing refresh token
    
    * fixing logout and refresh token
    
    * fixed logout
    
    * rebasing the branch
    
    * reducing lines of codes

--- src/controllers/userController/refreshToken.js
+++ src/controllers/userController/refreshToken.js
@@ -5,16 +5,13 @@ import { verifyToken, generateToken } from '../../utils/auth';
 const refreshToken = async (req, res, next) => {
   try {
     const token = req.cookies.make;
-    try {
+   
       if (!token) {
         throw new BadRequestError(res.__('Please login!'), 400);
       }
-    } catch (e) {
-      next(e);
-    }
     const payload = await verifyToken(token);
     // check if user exist in databasa
-    const newUser = await UserServices.getUserByUserName(payload.username);
+    const newUser = await UserServices.getUserByUserNameAndToken(payload.username,token);
     const userData = {
       username: newUser.username,
       user_role_id: newUser.user_role_id
@@ -23,7 +20,10 @@ const refreshToken = async (req, res, next) => {
       throw new BadRequestError(res.__('no user found with this token'), 400);
     }
     const userToken = generateToken(userData);
-
+    
+    await newUser.update({ refreshtoken: userToken });
+    res.clearCookie('make', { path: '/api/v1/user/refresh-token' });
+   
     // user.reftoken = reftoken;
     res.cookie('make', userToken, { httpOnly: false, path: '/api/v1/user/refresh-token' });
     return res.status(200).json({ userToken });
