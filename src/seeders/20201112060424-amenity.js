3fe5e6ae4425f8908f35b266fbc111748bdd0a10
CYIMANA Faisal <cyimanafaisal@gmail.com>
2021-01-22 10:56:25 +0200

    Fx naming conflicts

--- src/seeders/20201112060424-amenity.js
+++ src/seeders/20201112060424-amenity.js
@@ -2,7 +2,7 @@ export default {
   up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Amenities', [
     {
       id: 'cf701c80-a729-4922-a04c-553015514a96',
-      AccommodationId: '0ce36391-2c08-3074-bddb-a4ea8cccbbc5',
+      accommodationId: '0ce36391-2c08-3074-bddb-a4ea8cccbbc5',
       wifi: false,
       airConditioner: true,
       shampoo: false,
