1cc4e7e7c84227a28d0f6945b974858266a58b0b
Blaiseniyo <blaiseniyonkuru12@gmail.com>
2021-03-10 16:16:45 +0200

    fixing some tests

--- src/controllers/admin/updateRoles.js
+++ src/controllers/admin/updateRoles.js
@@ -11,13 +11,11 @@ const updateRoles = async (req, res, next) => {
     try {
     const role= await models.Role.findOne({where:{id:id}});
     let perm=null;
-    if (roles.hasOwnProperty(role.name)) {
-      if(role.name != requestData.name){
+    if (roles.hasOwnProperty(role.name) && role.name != requestData.name) {
         perm=roles[role.name];
         if(delete roles[role.name]){
           roles[requestData.name]=perm;
         };
-      }
     }
     const update = await models.Role.update(requestData,{where:{id:id}});  
     const dataJson = JSON.stringify(roles, null, 2);
