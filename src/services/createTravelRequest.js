5221ff965ac41d3f70057a7e64ffde836645611e
Cyusa144 <69196120+Cyusa144@users.noreply.github.com>
2021-03-17 16:41:20 +0200

    adds pusher

--- src/services/createTravelRequest.js
+++ src/services/createTravelRequest.js
@@ -1,22 +1,9 @@
 import db from '../models'
-import pusher from '../config/pusher';
-const createTravelRequest = async (req, res, request, next) => {
 
+const createTravelRequest = async (req, res, request, next) => {
   db.TravelRequest.create(request)
     .then((tRequestData) => {
       try {
-        const newNotificantion = {
-          user_id: tRequestData.managerId,
-          title: 'your user has made a Travel Request',
-          message: "The user you are assigned to has made a travel request  ",
-          travelId:tRequestData.travelId
-          };
-            
-         db.Notification.create(newNotificantion).then(notification=>{
-          pusher.trigger('bare-foot-normad', 'notification', notification);
-          console.log(notification)
-         });
-        
         let counter = req.body.trip.length;
         for (const record of req.body.trip) {
           counter -= 1;
@@ -25,7 +12,7 @@ const createTravelRequest = async (req, res, request, next) => {
             db.Trip.bulkCreate(req.body.trip, { hooks: true })
               .then((tripData) => {
                 const allData = { ...tRequestData.get({ plain: true }), tripData };
-                // res.json({ message: ('Trip request sent successfully'), data: allData });
+                res.json({ message: ('Trip request sent successfully'), data: allData });
                 next();
               })
               .catch((err) => { next(err); });
