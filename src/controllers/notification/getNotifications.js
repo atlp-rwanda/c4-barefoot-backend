b8b4557406f1bbac3aa42c61837081993f4021b0
Cyusa144 <69196120+Cyusa144@users.noreply.github.com>
2021-03-19 08:54:40 +0200

    adds pusher config and sorts notifications

--- src/controllers/notification/getNotifications.js
+++ src/controllers/notification/getNotifications.js
@@ -4,31 +4,60 @@ import models from '../../models';
 import NotFoundRequestError from '../../utils/Errors/notFoundRequestError';
 import { verifyToken } from '../../utils/auth';
 import isUserExist from '../../services/findUserByUsername';
+import { Op } from 'sequelize'
 
-const notific = async (req, res) => {
+export const readedNotific = async (req, res) => {
   const userData = req.headers.authorization;
   if (!userData) return res.status(401).json({ status: 401, message: ('Unauthorized, Please login!') });
   const token = userData.split(' ')[1];
   const decoded = await verifyToken(token);
   const user = await isUserExist(decoded.username);
+  console.log(user)
   try {
-    const [count, unread] = await models.Notification.findAndCountAll({
-      where: {
-        user_id: user.id,
-        status: "not readed"
-      }
-    })
+   
 
-    const notifications = await models.Notification.find({
+    const notifications = await models.Notification.findAndCountAll({
       where: { 
-        user_id: user.id,
-        status: 'readed'
-      }
+        [Op.and]: {
+          user_id: user.id,
+          status: 'readed'
+        }
+       
+      },
+      order: ['createdAt', 'DESC']
     });
-    console.log(count);
-    return res.status(200).json({ status: 200, message: ('user\'s Notifications'), readNotifications:notifications,unreadCount:count, unReadNotifications:unread });
+    return res.status(200).json({ status: 200, message: ('user\'s Notifications'), notifications });
   } catch (error) {
-    res.json(error.message, error.stack);
+    console.log('catch')
+    res.json(error);
   }
 };
-export default notific;
+
+export cssonst unReadedNotific = async (req, res)=>{
+  const userData = req.headers.authorization;
+  if (!userData) return res.status(401).json({ status: 401, message: ('Unauthorized, Please login!') });
+  const token = userData.split(' ')[1];
+  const decoded = await verifyToken(token);
+  const user = await isUserExist(decoded.username);
+  console.log(user)
+  try {
+   
+
+    const notifications = await models.Notification.findAndCountAll({
+      where: { 
+        [Op.and]: {
+          user_id: user.id,
+          status: 'not readed'
+        }
+       
+      },
+      order: [
+        ['createdAt', 'DESC']
+      ]
+    });
+    return res.status(200).json({ status: 200, message: ('user\'s Notifications'), notifications });
+  } catch (error) {
+    console.log('catch')
+    res.json(error);
+  }
+}
