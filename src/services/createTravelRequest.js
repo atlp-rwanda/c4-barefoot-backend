3657b84ae974033167bc2adeb32254263ccc102c
Cyusa144 <69196120+Cyusa144@users.noreply.github.com>
2021-03-21 20:39:18 +0200

    getting single notification

--- src/services/createTravelRequest.js
+++ src/services/createTravelRequest.js
@@ -1,9 +1,22 @@
 import db from '../models'
-
+import pusher from '../config/pusher';
 const createTravelRequest = async (req, res, request, next) => {
+
   db.TravelRequest.create(request)
     .then((tRequestData) => {
       try {
+        const newNotificantion = {
+          user_id: tRequestData.managerId,
+          title: 'your user has made a Travel Request',
+          message: "The user you are assigned to has made a travel request  ",
+          travelId:tRequestData.travelId
+          };
+            
+         db.Notification.create(newNotificantion).then(notification=>{
+          pusher.trigger('bare-foot-normad', 'notification', notification);
+          console.log(notification)
+         });
+        
         let counter = req.body.trip.length;
         for (const record of req.body.trip) {
           counter -= 1;
@@ -12,7 +25,7 @@ const createTravelRequest = async (req, res, request, next) => {
             db.Trip.bulkCreate(req.body.trip, { hooks: true })
               .then((tripData) => {
                 const allData = { ...tRequestData.get({ plain: true }), tripData };
-                res.json({ message: ('Trip request sent successfully'), data: allData });
+                // res.json({ message: ('Trip request sent successfully'), data: allData });
                 next();
               })
               .catch((err) => { next(err); });
